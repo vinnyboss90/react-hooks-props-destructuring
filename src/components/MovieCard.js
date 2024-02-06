e66d9023bde022058b83fceaeaf683db16b31823
vinnyegon90 <vinnyegon90@gmail.com>
2024-02-06 13:07:06 +0300

    Done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+function MovieCard(props) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
-      <h2>{title}</h2>
-      <small>{genres.join(", ")}</small>
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
     </div>
-  );
+  )
 }
 
 export default MovieCard;
